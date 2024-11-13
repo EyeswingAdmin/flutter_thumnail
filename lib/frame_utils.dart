5524c4d678e6c8f1f780d9904eb748cff718abaa
viktorknem <viktor.knops@gmail.com>
2024-11-13 11:21:47 +0000

    code migration

---
+++ lib/frame_utils.dart
@@ -0,0 +1,96 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:video_thumbnail/video_thumbnail.dart';
+
+import 'dart:isolate';
+import 'dart:async';
+
+class FrameUtils {
+  // Get a thumbnail image from a video at a specified position.
+  Future<Uint8List?> getThumbnail(String videoPath,
+      {Duration position = Duration.zero}) async {
+    try {
+      final uint8list = await VideoThumbnail.thumbnailData(
+        video: videoPath,
+        imageFormat: ImageFormat.JPEG,
+        quality: 50,
+        timeMs: position.inMilliseconds,
+      );
+      return uint8list;
+    } catch (e, s) {
+      // Handle any exceptions and print error information.
+      debugPrint(e.toString());
+      debugPrintStack(stackTrace: s);
+      return null;
+    }
+  }
+
+  // Get a list of thumbnails from a video at evenly spaced intervals.
+  Future<List<Uint8List>> getListThumbnail({
+    required String videoPath,
+    required Duration duration,
+    required int split,
+  }) async {
+    final jumpStep = duration.inSeconds / split ~/ 1;
+    final List<Duration> timePoint = [];
+
+    for (int second = 0; second < duration.inSeconds; second += jumpStep) {
+      timePoint.add(Duration(seconds: second));
+    }
+    final listThumbnail = await Future.wait(timePoint
+        .map((duration) => getThumbnail(videoPath, position: duration)));
+    return listThumbnail.whereType<Uint8List>().toList();
+  }
+
+  // Generate thumbnails in an isolate and send them back to the main isolate.
+  Future<void> generateThumbnails(Map<String, dynamic> data) async {
+    // Ensure that the isolate's binary messenger is initialized.
+    BackgroundIsolateBinaryMessenger.ensureInitialized(rootToken);
+    final videoPath = data['videoPath'];
+    final duration = data['duration'] as Duration;
+    final split = data['split'];
+    final jumpStep = (duration.inMilliseconds / split).ceil();
+    final List<Duration> timePoint = [];
+    for (int ms = 0; ms < duration.inMilliseconds; ms += jumpStep) {
+      timePoint.add(Duration(milliseconds: ms));
+    }
+
+    final List<Uint8List> listThumbnail = [];
+
+    for (var duration in timePoint) {
+      final thumbnail = await getThumbnail(videoPath, position: duration);
+      if (thumbnail != null) {
+        listThumbnail.add(thumbnail);
+      }
+    }
+
+    // Send the list of thumbnails back to the main isolate.
+    data['sendPort'].send(listThumbnail);
+  }
+
+  late RootIsolateToken rootToken;
+
+  // Get a list of thumbnails from a video using an isolate.
+  Future<List<Uint8List>> getListThumbnailIsolate({
+    required String videoPath,
+    required Duration duration,
+    required int split,
+  }) async {
+    final receivePort = ReceivePort();
+    rootToken = RootIsolateToken.instance!;
+    final isolateData = {
+      'videoPath': videoPath,
+      'duration': duration,
+      'split': split,
+      'sendPort': receivePort.sendPort,
+    };
+
+    await Isolate.spawn(generateThumbnails, isolateData);
+
+    final List<Uint8List> listThumbnail = await receivePort.first;
+
+    receivePort.close();
+
+    return listThumbnail;
+  }
+}
---
+++ lib/video_thumbnail_slider.dart
@@ -0,0 +1,228 @@
+library video_thumbnail_slider;
+
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:video_player/video_player.dart';
+import 'package:video_thumbnail_slider/frame_utils.dart';
+
+class VideoThumbnailSlider extends StatefulWidget {
+  /// The controller for the video that this slider is controlling.
+  final VideoPlayerController controller;
+
+  /// The height of the video thumbnail slider.
+  final double height;
+
+  /// The width of the video thumbnail slider.
+  final double width;
+
+  /// The number of split images in the thumbnail slider.
+  final int splitImage;
+
+  /// The background color of the slider.
+  final Color backgroundColor;
+
+  /// A custom builder for the current frame of the video.
+  final Widget Function(VideoPlayerController controller)? customCurrentFrameBuilder;
+
+  /// A builder for the individual frames in the slider.
+  final Widget Function(Uint8List imageData)? frameBuilder;
+
+  /// A listener for the video position changes.
+  final void Function(Duration position)? onPositionChanged;
+
+  /// The color of the current frame border.
+  final Color currentFrameBorderColor;
+
+  /// Constructor for VideoThumbnailSlider.
+  const VideoThumbnailSlider({
+    Key? key,
+    required this.controller,
+    this.height = 50,
+    this.width = 350,
+    this.splitImage = 7,
+    this.customCurrentFrameBuilder,
+    this.frameBuilder,
+    this.backgroundColor = Colors.black,
+    this.onPositionChanged,
+    this.currentFrameBorderColor = Colors.white,
+  }) : super(key: key);
+
+  @override
+  VideoThumbnailSliderState createState() => VideoThumbnailSliderState();
+}
+
+class VideoThumbnailSliderState extends State<VideoThumbnailSlider> {
+  // The current position of the slider.
+  double _slidePosition = 0.0;
+
+  // The height of the video thumbnail slider.
+  late final _height = widget.height;
+
+  // The width of the video thumbnail slider.
+  late final _width = widget.width;
+
+  // The number of split images in the thumbnail slider.
+  late final _splitImage = widget.splitImage;
+
+  // The video controller for the video being displayed.
+  late final _videoController = widget.controller;
+
+  // Callback function when the video position changes.
+  void _onPositionChanged() async {
+    if (_videoController.value.isPlaying) {
+      final position = (await _videoController.position) ?? Duration.zero;
+      setState(() {
+        _slidePosition = position.inMilliseconds / _videoController.value.duration.inMilliseconds;
+      });
+      if (widget.onPositionChanged != null) {
+        widget.onPositionChanged!(position);
+      }
+      return;
+    }
+
+    final position = (await _videoController.position) ?? Duration.zero;
+
+    if (position.inMilliseconds > _videoController.value.duration.inMilliseconds) {
+      setState(() {
+        _slidePosition = 1;
+      });
+    }
+    if (widget.onPositionChanged != null) {
+      widget.onPositionChanged!(position);
+    }
+  }
+
+  // Callback function when the user drags the slider horizontally.
+  void _onHorizontalDragSlider(DragUpdateDetails details) async {
+    setState(() {
+      // Calculate the new position of the slide rectangle based on drag.
+      _slidePosition += details.delta.dx / context.size!.width;
+      _slidePosition = _slidePosition.clamp(0.0, 1.0);
+    });
+    // final second = widget.controller.value.duration.inSeconds;
+    final ms = widget.controller.value.duration.inMilliseconds;
+    // await widget.controller.pause();
+    widget.controller.seekTo(Duration(milliseconds: (ms * _slidePosition).ceil()));
+    // .seekTo(Duration(seconds: (second * _slidePosition).ceil()));
+  }
+
+  @override
+  void initState() {
+    // Add a listener to the video controller for position changes.
+    _videoController.addListener(() {
+      _onPositionChanged();
+    });
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      width: _width,
+      height: _height,
+      child: Stack(
+        children: [
+          Container(
+            width: _width,
+            height: _height,
+            color: widget.backgroundColor,
+            child: BackgroundSlider(
+              controller: _videoController,
+              splitThumb: _splitImage,
+              frameBuilder: widget.frameBuilder,
+            ),
+          ),
+          Positioned(
+            left: (_width - _width / _splitImage) * _slidePosition,
+            child: GestureDetector(
+              onHorizontalDragUpdate: _onHorizontalDragSlider,
+              child: SizedBox(
+                width: _width / _splitImage,
+                height: _height,
+                child: Container(
+                  decoration: BoxDecoration(
+                    border: Border.all(color: widget.currentFrameBorderColor, width: 3),
+                    color: Colors.transparent,
+                  ),
+                  child: widget.customCurrentFrameBuilder?.call(widget.controller),
+                ),
+              ),
+            ),
+          )
+        ],
+      ),
+    );
+  }
+}
+
+class BackgroundSlider extends StatefulWidget {
+  const BackgroundSlider({
+    required this.controller,
+    required this.splitThumb,
+    this.frameBuilder,
+    Key? key,
+  }) : super(key: key);
+
+  // The video controller for the video being displayed.
+  final VideoPlayerController controller;
+
+  // The number of split images in the thumbnail slider.
+  final int splitThumb;
+
+  // A builder for the individual frames in the slider.
+  final Widget Function(Uint8List)? frameBuilder;
+
+  @override
+  State<BackgroundSlider> createState() => _BackgroundSliderState();
+}
+
+class _BackgroundSliderState extends State<BackgroundSlider> {
+  // The video controller for the video being displayed.
+  late final VideoPlayerController _videoController = widget.controller;
+
+  // List of thumbnail images.
+  List<Uint8List> listThumbnail = [];
+
+  // Flag indicating whether the list of thumbnails has been generated.
+  bool hasListThumbnail = false;
+
+  @override
+  void initState() {
+    if (_videoController.value.isInitialized) {
+      // Generate the list of thumbnails if the video is initialized.
+      getListThumbnail();
+    } else {
+      // Add a listener to the video controller to generate thumbnails when it's initialized.
+      _videoController.addListener(() {
+        if (_videoController.value.isInitialized && !hasListThumbnail) {
+          getListThumbnail();
+        }
+      });
+    }
+    super.initState();
+  }
+
+  // Generate the list of thumbnails from the video.
+  void getListThumbnail() async {
+    hasListThumbnail = true;
+    final result = await FrameUtils().getListThumbnailIsolate(videoPath: _videoController.dataSource, duration: _videoController.value.duration, split: widget.splitThumb);
+    setState(() {
+      listThumbnail = result;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      crossAxisAlignment: CrossAxisAlignment.stretch,
+      children: listThumbnail
+          .map((imgData) => Expanded(
+              child: widget.frameBuilder?.call(imgData) ??
+                  Image.memory(
+                    imgData,
+                    fit: BoxFit.fitWidth,
+                  )))
+          .toList(),
+    );
+  }
+}
